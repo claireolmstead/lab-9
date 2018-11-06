4ffe7dc82a29964b54e28266f368193104703095
V. Claire Olmstead <claireolmstead@cu-vpn-colorado-edu-198.11.30.124.int.colorado.edu>
2018-11-05 20:03:20 -0700

    make it better

---
+++ database.js
@@ -0,0 +1,16 @@
+
+
+r pgp = require('pg-promise')();
+
+const dbConfig = {
+   host: 'localhost',
+   port: 5432,
+   database: 'lab6',
+   user: 'postgres',
+   password: 'Roxy' // TODO: Fill in your PostgreSQL password here.
+                // Use empty string if you did not set a password
+};
+
+var db = pgp(dbConfig);
+
+module.exports = db;
